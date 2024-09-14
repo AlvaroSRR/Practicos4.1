7693f04c95499ca2a1bce669168f0a096fa76521
Alvaro <alvaro.srr@gmail.com>
2024-09-14 18:17:47 -0300

    ejercicio 24 sin espacios y prueba de escritorio en pdf

--- ejercicio24.py
+++ ejercicio24.py
@@ -3,6 +3,5 @@ precioCombustible=float(input("ingrese el precio del combustible: "))
 kmsXlitro=float(input("ingrese los KMS recorridos por litro de combustible: "))
 combustibleConsumido=float(kmsRecorridos/kmsXlitro)
 importeCombustible=float(combustibleConsumido*precioCombustible)
-
 print(f"Litros de combustible consumidos: {combustibleConsumido}")
 print(f"Importe gastado en combustible: {importeCombustible}")
